3036dbd6a7b38df6ab0ef24f3cc23aa8602b7d6b
QuanHNguyen232 <quan.nh2002@gmail.com>
2024-07-29 01:41:57 +0700

    add my info

---
+++ src/content_option.js
@@ -0,0 +1,163 @@
+const logotext = "Quan";
+const meta = {
+    title: "Quan Nguyen",
+    description: "I'm Quan Nguyen, MSCS student at UMD",
+};
+
+const introdata = {
+    title: "I'm Quan Nguyen",
+    animated: {
+        first: "I love AI",
+        second: "I code web apps with AI integrated",
+    },
+    description: "I'm Quan Nguyen from Vietnam. I'm a MSCS student at UMD",
+    your_img_url: "https://github.com/QuanHNguyen232/quanhnguyen232.github.io/blob/master/images/anh-165.png?raw=true",
+};
+
+const dataabout = {
+    title: "a bit about myself",
+    aboutme: "I'm Quan Nguyen from Vietnam. I'm a MSCS student at UMD.",
+};
+const worktimeline = [{
+        jobtitle: "Machine Learning Engineer Intern",
+        where: "Vietnam",
+        date: "2024",
+    },
+    {
+        jobtitle: "Machine Learning Research Intern",
+        where: "MSU, East Lansing, MI",
+        date: "2023",
+    },
+    {
+        jobtitle: "Machine Learning Research Intern",
+        where: "Purdue, West Lafayette, IN",
+        date: "2022",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 60,
+    },
+    {
+        name: "Java",
+        value: 75,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://wallpapercave.com/wp/wp8903890.jpg",
+        description: "MERN app with RAG for Summer Research program support.",
+        link: "https://github.com/QuanHNguyen232/REU_RAG",
+    },
+    {
+        img: "https://wallpaperaccess.com/full/1533564.png",
+        description: "Fine-tuning BERT for Named Entity Recognition on eBay's dataset.",
+        link: "https://github.com/QuanHNguyen232/eBay_ML_Challenge_2022",
+    }
+    // {
+    //     img: "https://picsum.photos/400/800/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/550/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "quanhnguyen232@gmail.com",
+    YOUR_FONE: "(717)420-6671",
+    description: "",
+    // create an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofiles = {
+    github: "https://github.com/QuanHNguyen232",
+    // facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/quanhnguyen232/",
+    // twitter: "https://twitter.com",
+    email: "quanhnguyen232@gmail.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofiles as socialprofils,
+    logotext,
+};
